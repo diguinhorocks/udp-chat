75183b871929fd6b5843f33538e5d5f1531878aa
Rodrigo Santiago <rsantiago897@gmail.com>
2013-10-04 07:18:47 -0300

    verificando usuario autenticado, enviando usuario pra view e modificações

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,49 @@
+var config = require('../config.json')
+	, app = module.parent.exports.app
+	, passport = require('passport');
 /*
  * Homepage
  */
-exports.main = function(req, res) {
-	res.render('client');
+exports.main = function(req, res, next) {
+	if(req.user) {
+		console.log(req.user);
+		res.render('client', { user: req.user.username });
+	}
+	else
+		res.render('login');
 }
+
+exports.login = function(req, res) {
+	res.render('login');
+}
+
+/*
+ * Authentication routes
+ */
+
+if(config.auth.twitter.consumerkey.length) {
+  app.get('/auth/twitter', passport.authenticate('twitter'));
+
+  app.get('/auth/twitter/callback', 
+    passport.authenticate('twitter', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    })
+  );
+}
+
+if(config.auth.facebook.clientid.length) {
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', 
+    passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/'
+    })
+  );
+}
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
